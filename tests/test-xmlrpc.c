bc9af58dc7f47ca15d986d41d039dac3dc532bc6
C.H. Huang <plushuang.tw@gmail.com>
2012-08-10 23:00:55 +0800

    Update test program : test-xmlrpc.c

--- tests/test-xmlrpc.c
+++ tests/test-xmlrpc.c
@@ -79,34 +79,107 @@ void ug_xmlrpc_value_dump (UgXmlrpcValue* value)
 	}
 }
 
-void test_xmlrpc (void)
+void aria2_getVersion (UgXmlrpc* xmlrpc)
 {
-	UgXmlrpc			xmlrpc;
 	UgXmlrpcValue*		value;
 	UgXmlrpcResponse	response;
 
-	ug_xmlrpc_init (&xmlrpc);
-	ug_xmlrpc_use_client (&xmlrpc, "http://localhost:6800/rpc", NULL);
-
 	g_print ("aria2.getVersion\n");
-	response = ug_xmlrpc_call (&xmlrpc, "aria2.getVersion", UG_XMLRPC_NONE);
-	if (response != UG_XMLRPC_ERROR) {
-		value = ug_xmlrpc_get_value (&xmlrpc);
-		ug_xmlrpc_value_dump (value);
-	}
+	response = ug_xmlrpc_call (xmlrpc,
+			"aria2.getVersion",
+			UG_XMLRPC_NONE);
+	switch (response) {
+	case UG_XMLRPC_ERROR:
+		g_print ("- error\n");
+		break;
 
-	g_print ("------\n");
+	case UG_XMLRPC_FAULT:
+	case UG_XMLRPC_OK:
+		value = ug_xmlrpc_get_value (xmlrpc);
+		ug_xmlrpc_value_dump (value);
+		break;
+	}
+	g_print ("\n");
+}
+
+void aria2_tellStatus (UgXmlrpc* xmlrpc)
+{
+	UgXmlrpcValue*		value;
+	UgXmlrpcResponse	response;
 
 	g_print ("aria2.tellStatus\n");
-	response = ug_xmlrpc_call (&xmlrpc, "aria2.tellStatus",
+	response = ug_xmlrpc_call (xmlrpc,
+			"aria2.tellStatus",
 			UG_XMLRPC_STRING, "1",
 			UG_XMLRPC_NONE);
-	if (response != UG_XMLRPC_ERROR) {
-		value = ug_xmlrpc_get_value (&xmlrpc);
-		ug_xmlrpc_value_dump (value);
-	}
+	switch (response) {
+	case UG_XMLRPC_ERROR:
+		g_print ("- error\n");
+		break;
 
-	ug_xmlrpc_finalize (&xmlrpc);
+	case UG_XMLRPC_FAULT:
+	case UG_XMLRPC_OK:
+		value = ug_xmlrpc_get_value (xmlrpc);
+		ug_xmlrpc_value_dump (value);
+		break;
+	}
+	g_print ("\n");
+}
+
+void aria2_changeGlobalOption (UgXmlrpc* xmlrpc)
+{
+	UgXmlrpcValue*		options;
+	UgXmlrpcValue*		member;
+	UgXmlrpcValue*		value;
+	UgXmlrpcResponse	response;
+
+	g_print ("aria2.changeGlobalOption\n");
+	options = ug_xmlrpc_value_new_struct (2);
+	// max-overall-download-limit
+	member = ug_xmlrpc_value_alloc (options);
+	member->name = "max-overall-download-limit";
+	member->type = UG_XMLRPC_STRING;
+	member->c.string = g_strdup_printf ("%dK", 0);
+	// max-overall-upload-limit
+	member = ug_xmlrpc_value_alloc (options);
+	member->name = "max-overall-upload-limit";
+	member->type = UG_XMLRPC_STRING;
+	member->c.string = g_strdup_printf ("%dK", 0);
+
+	response = ug_xmlrpc_call (xmlrpc,
+			"aria2.changeGlobalOption",
+			UG_XMLRPC_STRUCT, options,
+			UG_XMLRPC_NONE);
+	switch (response) {
+	case UG_XMLRPC_ERROR:
+		g_print ("- error\n");
+		break;
+
+	case UG_XMLRPC_FAULT:
+	case UG_XMLRPC_OK:
+		value = ug_xmlrpc_get_value (xmlrpc);
+		ug_xmlrpc_value_dump (value);
+		break;
+	}
+	g_print ("\n");
+
+	ug_xmlrpc_value_free (options);
+}
+
+void test_xmlrpc (void)
+{
+	UgXmlrpc*			xmlrpc;
+
+	xmlrpc = g_malloc0 (sizeof (UgXmlrpc));
+	ug_xmlrpc_init (xmlrpc);
+	ug_xmlrpc_use_client (xmlrpc, "http://localhost:6800/rpc", NULL);
+
+	aria2_getVersion (xmlrpc);
+	aria2_tellStatus (xmlrpc);
+	aria2_changeGlobalOption (xmlrpc);
+
+	ug_xmlrpc_finalize (xmlrpc);
+	g_free (xmlrpc);
 }
 
 #endif	// HAVE_PLUGIN_ARIA2
